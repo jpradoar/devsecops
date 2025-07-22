3679c53bd7c0a1fc9a5c3fdef48956fed53bf60f
Jonathan Prado <jpradoar@gmail.com>
2025-07-22 11:47:25 -0400

    feat/infra-cicd: add infra validation

---
+++ terraform/sg.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "sg_allow_ssh_demo"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "All for demos"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
