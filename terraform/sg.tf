537ceca6bf0b890e4a1e951285b755f9b3510959
Jonathan Prado <jpradoar@gmail.com>
2025-07-30 21:20:13 +0000

    fix: fake triggering workflow

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -8,13 +8,13 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["99.99.99.99/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["99.99.99.99/32"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 }
